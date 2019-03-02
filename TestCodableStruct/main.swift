082c1476844bf3ba758da734988cd721ad25528e
liumin_mbp <317590898@qq.com>
2019-03-02 15:51:23 +0800

    Initial Commit

---
+++ TestCodableStruct/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TestCodableStruct
+//
+//  Created by Bill liu on 2019/3/2.
+//  Copyright © 2019 Mzying. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
